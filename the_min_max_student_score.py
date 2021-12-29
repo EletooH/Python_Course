ce736d3b761d1915cbb1d9200e8e0c141315bb5f
EletooH <94681626+EletooH@users.noreply.github.com>
2021-12-29 10:44:13 +0200

    Update the_min_max_student_score.py

--- the_min_max_student_score.py
+++ the_min_max_student_score.py
@@ -1,8 +1,13 @@
-students_score = [
-    ['Ivan', 5.00],
-    ['Alex', 3.50],
-    ['Maria', 5.50],
-    ['Georgy', 5.00]
-]
-print(min(students_score))
-print(max(students_score))
+students_score = {
+    'Ivan': 5.00,
+    'Alex': 3.50,
+    'Maria': 5.50,
+    'Georgy': 5.00
+}
+
+for name, score in students_score.items():
+    if name == max(students_score):
+        print ('{} {}'.format(name,score))
+for name, score in students_score.items():
+    if name == min(students_score):
+        print ('{} {}'.format(name,score))
